c180fc6a12bc094f464f972942064d6258096bbe
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-04-10 06:13:29 +0530

    Starter Project

---
+++ src/main/java/com/example/rest_api_crudjava_spring/RestApiCrudJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_crudjava_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCrudJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiCrudJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rest_api_crudjava_spring/RestApiCrudJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_crudjava_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
